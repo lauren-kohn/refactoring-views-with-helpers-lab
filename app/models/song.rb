870298e55f5631a2358dd8fed86b71d62b84c7c2
lauren-kohn <lauren.kohn@soft-adv.com>
2021-08-03 07:26:09 -0500

    commit tree 5eb4d82a6da0a91442eb24c578c2f2129a3f5f13

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,13 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name if artist
+    # if the artist's name exists, then deliver it
   end
 
   def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+    # order is important because otherwise nil is being set to artist
   end
 end
